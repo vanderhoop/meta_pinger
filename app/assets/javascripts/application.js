54eeb22115ee25d66c14bcce03e4f0ef0fea3985
Travis Vander Hoop <vanderhoop@me.com>
2014-01-17 19:16:55 -0500

    set project up for scss and wrote out examples of functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,42 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$( document ).ready(function() {
+$.fn.typewriter = function() {
+        this.each(function() {
+            var $ele = $(this), str = $ele.html(), progress = 0, offset = 0;
+            $ele.html('');
+            var typewriting = function() {
+
+              if(str.substring(progress, progress + 1) == "<"){
+                var re = new RegExp(/<span class="instant"/);
+                var cl = new RegExp(/<span class="clear"/);
+                if (str.substring(progress,str.length).match(re)) {
+                  progress += str.substring(progress,str.length).indexOf('</span>')+7;
+                }else if(str.substring(progress,str.length).match(cl)){
+                  offset = progress;
+                  progress += str.substring(progress,str.length).indexOf('</span>')+7;
+
+
+                }else{
+                while(str.substring(progress, progress + 1) != ">"){
+                  progress++;
+                }
+                }
+              }
+
+                $ele.html(str.substring(offset, progress++) + (progress & 1 ? '_' : ''));
+              if (progress >= str.length){
+                return;
+              }
+              else{
+                setTimeout(typewriting, 10 + Math.random()*100);
+              }
+            }
+            typewriting();
+        });
+        return this;
+    };
+$('#terminal').typewriter();
+});
