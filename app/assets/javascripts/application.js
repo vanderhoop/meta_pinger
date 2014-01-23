5995b451b679314d384fa2a2cc3af64d3a3be7fe
Travis Vander Hoop <vanderhoop@me.com>
2014-01-23 08:35:27 -0500

    altered styling on text-field

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,40 +15,8 @@
 //= require_tree .
 
 $( document ).ready(function() {
-$.fn.typewriter = function() {
-        this.each(function() {
-            var $ele = $(this), str = $ele.html(), progress = 0, offset = 0;
-            $ele.html('');
-            var typewriting = function() {
 
-              if(str.substring(progress, progress + 1) == "<"){
-                var re = new RegExp(/<span class="instant"/);
-                var cl = new RegExp(/<span class="clear"/);
-                if (str.substring(progress,str.length).match(re)) {
-                  progress += str.substring(progress,str.length).indexOf('</span>')+7;
-                }else if(str.substring(progress,str.length).match(cl)){
-                  offset = progress;
-                  progress += str.substring(progress,str.length).indexOf('</span>')+7;
-
-
-                }else{
-                while(str.substring(progress, progress + 1) != ">"){
-                  progress++;
-                }
-                }
-              }
-
-                $ele.html(str.substring(offset, progress++) + (progress & 1 ? '_' : ''));
-              if (progress >= str.length){
-                return;
-              }
-              else{
-                setTimeout(typewriting, 10 + Math.random()*100);
-              }
-            }
-            typewriting();
-        });
-        return this;
-    };
-$('#terminal').typewriter();
+  $('#app_url').on("click", function(){
+    $(this).addClass("activated");
+  });
 });
