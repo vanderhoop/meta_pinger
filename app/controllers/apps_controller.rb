3344da15d63595f0af5ed0e4b5dba0032257c1a1
Travis Vander Hoop <vanderhoop@me.com>
2014-02-07 15:28:22 -0500

    app now handles cases where users include 'http://' or 'http://www.' at the head of the url

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,20 +1,27 @@
 class AppsController < ApplicationController
 
   def create
+    # grabs the url
     submitted_url = params["app"]["url"]
+    # handles cases where users include 'http://'' or 'http://www.'
+    submitted_url.gsub!('http://www.', '')
+    submitted_url.gsub!('http://', '')
+    # chops off unecessary trailing forward slash
     submitted_url = submitted_url[0..-2] if submitted_url.last == "/"
-    response = HTTParty.get("http://" + submitted_url)
-    error = response.include?("No such app") ? "Heroku says there's no app at this location. Maybe check for typos." : "No error here"
+    ending_string = submitted_url.slice(-13,13)
 
-    # If the supplied URL has a forward slash appended,
-    # I chop it off
-    ending_string = params["app"]["url"].slice(-13,13)
-    if params["app"]["url"].slice(-13,13) == "herokuapp.com"
-      redirect_to '/success' if App.create(params["app"])
+    if ending_string == "herokuapp.com"
+      response = HTTParty.get("http://" + submitted_url)
+      if response.include?("No such app")
+        flash[:error] = "ERROR: Heroku says there's no app at this location. Maybe check for typos and resubmit."
+        redirect_to '/'
+      else
+        redirect_to '/success' if App.create(params["app"])
+      end
     else
-      flash[:error] = "You supplied an unworthy URL. Make sure the URL ends in herokuapp.com."
+      flash[:error] = "ERROR: It seems you supplied an unworthy URL. Make sure the URL ends in 'herokuapp.com.'"
       redirect_to '/'
-    end
-  end
+    end # if ending_string == "herokuapp.com"
 
+  end # create
 end
