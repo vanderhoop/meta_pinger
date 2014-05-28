a768f9c4d31abcff5587eed3770db816fdd0966e
vanderhoop <vanderhoop@me.com>
2014-05-27 20:49:33 -0400

    previously submitted apps are redirected

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -15,13 +15,19 @@ class AppsController < ApplicationController
       if response.include?("No such app")
         flash[:error] = "ERROR: Heroku says there's no app at this location. Maybe check for typos and resubmit."
         redirect_to '/'
-      else
-        redirect_to '/success' if App.create(params["app"])
       end
+
+      app = App.new(params["app"])
+      if app.save
+        redirect_to '/success'
+      elsif app.errors.full_messages.include?("Url has already been taken")
+        flash[:error] = "Hmmm... looks like MetaPinger is already pinging this app."
+        redirect_to '/'
+      end
+
     else
       flash[:error] = "ERROR: It seems you supplied an unworthy URL. Make sure the URL ends in 'herokuapp.com.'"
       redirect_to '/'
-    end # if ending_string == "herokuapp.com"
-
-  end # create
+    end
+  end
 end
