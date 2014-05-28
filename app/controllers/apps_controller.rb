fae4fc3645f0bb8bdd275145a8d65ff14fab5726
vanderhoop <vanderhoop@me.com>
2014-05-27 22:21:03 -0400

    alphabetized scss style declarations and added 'and return' to redirects

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,19 +1,14 @@
 class AppsController < ApplicationController
 
   def create
-    # grabs the url
-    submitted_url = params["app"]["url"]
-    valid_heroku_url = validate_submitted_url(submitted_url)
+    heroku_url = validate_submitted_url(params["app"]["url"])
 
-    if !valid_heroku_url
-      flash[:error] = "ERROR: It seems you supplied an unworthy URL. Make sure the URL ends in 'herokuapp.com.'"
-      redirect_to '/'
-    else
-      url_tail = prepare_url_for_request(valid_heroku_url)
+    if heroku_url
+      url_tail = prepare_url_for_request(heroku_url)
       response = HTTParty.get("http://" + url_tail)
       if response.include?("No such app")
         flash[:error] = "ERROR: Heroku says there's no app at this location. Maybe check for typos and resubmit."
-        redirect_to '/'
+        redirect_to '/' and return
       end
 
       app = App.new(params["app"])
@@ -21,8 +16,11 @@ class AppsController < ApplicationController
         redirect_to '/success'
       elsif app.errors.full_messages.include?("Url has already been taken")
         flash[:notice] = "Hmmm... looks like MetaPinger is already pinging this app, so rest easy."
-        redirect_to '/'
+        redirect_to '/' and return
       end
+    else
+      flash[:error] = "ERROR: It seems you supplied an unworthy URL. Make sure the URL ends in 'herokuapp.com.'"
+      redirect_to '/' and return
     end
   end
 end
