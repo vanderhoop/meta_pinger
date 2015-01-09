ea69a7355129785484b189353491f132dfd7ee6f
vanderhoop <vanderhoop@me.com>
2015-01-08 23:34:54 -0500

    fixes bug for urls entered with leading whitespace

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   def validate_submitted_url(submitted_url)
-    regex_match = /^(https?:\/\/)?(w{3}\.)?(\S+)(\.herokuapp\.com)\/?$/i.match(submitted_url)
+    regex_match = /^(https?:\/\/)?(w{3}\.)?(\S+)(\.herokuapp\.com)\/?$/i.match(submitted_url.strip)
   end
 
   def prepare_url_for_request(regex)
