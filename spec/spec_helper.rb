513ed03e33d19e0c358c77685c96c78f01885685
vanderhoop <vanderhoop@me.com>
2015-01-17 20:10:51 -0500

    sets enforcement of available locales to false to quiet deprecation warning

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,9 @@ require 'rspec/autorun'
 require 'capybara/rails'
 require 'capybara/rspec'
 
+# quiets deprecation warning
+I18n.enforce_available_locales = false
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
