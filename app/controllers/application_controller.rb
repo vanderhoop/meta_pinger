bc52972fd4a457fefb3bf5807d776f97c3f4785e
vanderhoop <vanderhoop@me.com>
2015-01-19 12:25:35 -0500

    removes obselete methods from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  def validate_submitted_url(submitted_url)
-    regex_match = /^(https?:\/\/)?(w{3}\.)?(\S+)(\.herokuapp\.com)\/?$/i.match(submitted_url.strip)
-  end
 
-  def prepare_url_for_request(regex)
-    regex[3] + regex[4]
-  end
 end
